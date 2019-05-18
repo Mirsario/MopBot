45e5435b5550ed7b1eb39440ef50497e4709b1f7
Mirsario <me@mirsar.io>
2019-05-18 08:28:27 +0300

    More array TypeReaders

--- src/TypeReaders/SocketTextChannelArrayTypeReader.cs
+++ src/TypeReaders/SocketTextChannelArrayTypeReader.cs
@@ -10,43 +10,16 @@ using Discord.WebSocket;
 
 namespace MopBotTwo.TypeReaders
 {
-	public class SocketTextChannelArrayTypeReader : CustomTypeReader
+	public class SocketTextChannelArrayTypeReader : DiscordEntityArrayTypeReader<SocketTextChannel>
 	{
 		public override Type[] Types => new[] { typeof(IChannel[]),typeof(ITextChannel[]),typeof(SocketTextChannel[]) };
-		
-		public static Regex channelRegex = new Regex(@"(?:<#(\d+)>|(\d+)|#([\w-]+))\s*",RegexOptions.Compiled);
-		
-		public override async Task<TypeReaderResult> ReadAsync(ICommandContext context,string input,IServiceProvider services)
-		{
-			var matches = channelRegex.Matches(input);
 
-			if(matches.Count==0) {
-				return TypeReaderResult.FromError(CommandError.ParseFailed,"Parse failure. Expected a list of channels.");
-			}
+		protected Regex parseRegex;
+		public override Regex ParseRegex => parseRegex ?? (parseRegex = new Regex($@"(?:(<\#\d+>|\d+)|#([\w-]+))\s*",RegexOptions.Compiled));
 
-			var channels = new List<SocketTextChannel>();
-
-			foreach(Match match in matches) {
-				var groups = match.Groups;
-				string idStr = groups[1].Value ?? groups[2].Value;
-				string anyStr = idStr ?? groups[3].Value;
-				 
-				SocketTextChannel channel = null;
-
-				if(idStr==null) {
-					channel = (await context.Guild.GetTextChannelsAsync()).FirstOrDefault(c => c.Name==anyStr) as SocketTextChannel;
-				}else if(ulong.TryParse(idStr,out ulong id)) {
-					channel = await context.Guild.GetTextChannelAsync(id) as SocketTextChannel;
-				}
-
-				if(channel==null) {
-					return TypeReaderResult.FromError(CommandError.ParseFailed,$"Invalid channel: `{anyStr}`.");
-				}
-
-				channels.Add(channel);
-			}
-
-			return TypeReaderResult.FromSuccess(channels.ToArray());
-		}
+		public override async Task<SocketTextChannel> GetFromId(ICommandContext context,ulong id)
+			=> (SocketTextChannel)await context.Guild.GetTextChannelAsync(id);
+		public override async Task<SocketTextChannel> GetFromName(ICommandContext context,string name)
+			=> (SocketTextChannel)(await context.Guild.GetTextChannelsAsync()).FirstOrDefault(c => MopBot.StrComparerIgnoreCase.Equals(c.Name,name));
 	}
 }
