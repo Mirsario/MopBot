f41a0164f3f65cdbab744db5389b82878df01c7c
Mirsario <me@mirsar.io>
2019-05-18 08:29:33 +0300

    More ArrayUtils methods, to avoid using concurrent collections when not necessary.

--- src/Utilities/ArrayUtils.cs
+++ src/Utilities/ArrayUtils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MopBotTwo
 {
@@ -14,6 +16,25 @@ namespace MopBotTwo
 				array[length] = value;
 			}
 		}
+		public static void RemoveWhere<T>(ref T[] array,Func<T,bool> predicate)
+		{
+			List<T> list = null;
+			int offset = 0;
+
+			for(int i = 0;i<array.Length;i++) {
+				var element = array[i];
+				if(predicate(element)) {
+					if(list==null) {
+						list = array.ToList();
+					}
+					list.RemoveAt(i+offset--);
+				}
+			}
+
+			if(list!=null) {
+				array = list.ToArray();
+			}
+		}
 		public static void RemoveAt<T>(ref T[] array,int index)
 		{
 			int length = array.Length-1;
@@ -26,6 +47,36 @@ namespace MopBotTwo
 				newArray[i] = array[j];
 			}
 			array = newArray;
+		}
+
+		public static bool TryGetFirst<T>(T[] array,Predicate<T> predicate,out T result)
+		{
+			for(int i = 0;i<array.Length;i++) {
+				var element = array[i];
+				if(predicate(element)) {
+					result = element;
+					return true;
+				}
+			}
+
+			result = default;
+			return false;
+		}
+		public static void ModifyOrAddFirst<T>(ref T[] array,Predicate<T> predicate,Func<T> instancer,Action<T> action,bool createArrayIfNull = false)
+		{
+			T element;
+			if(array==null) {
+				if(!createArrayIfNull) {
+					throw new ArgumentNullException(nameof(array));
+				}
+				array = new T[1] {
+					element = instancer()
+				};
+			}else if(!TryGetFirst(array,predicate,out element)) {
+				element = instancer();
+			}
+
+			action(element);
 		}
 	}
 }
